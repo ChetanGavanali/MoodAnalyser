900287aaad620050c89fddb4c691720243b49df1
ChetanGavanali <chetangavanali@gmail.com>
2022-07-12 19:46:48 +0530

    Added RepeatTC1.2

--- src/test/java/com/moodanalyser/MoodAnalyserConstructorTest.java
+++ src/test/java/com/moodanalyser/MoodAnalyserConstructorTest.java
@@ -6,10 +6,18 @@ import org.junit.jupiter.api.Test;
 public class MoodAnalyserConstructorTest {
     @Test
     public void respondAsSadMoodWithConstructor() {
-        MoodAnalyserConstructor moodAnalyzer = new MoodAnalyserConstructor();
-        String result = moodAnalyzer.analyseMood("I am in sad Mood");
+        MoodAnalyserConstructor moodAnalyser = new MoodAnalyserConstructor();
+        String result = moodAnalyser.analyseMood("I am in sad Mood");
         System.out.println("Iam in " + result + " mood.");
         Assertions.assertEquals("SAD", result);
+    }
+
+    @Test
+    public void respondAsHappyMoodWithConstructor() {
+        MoodAnalyserConstructor moodAnalyser = new MoodAnalyserConstructor();
+        String result = moodAnalyser.analyseMood("I am in happy Mood");
+        System.out.println("Iam in " + result + " mood.");
+        Assertions.assertEquals("HAPPY", result);
     }
 }
 
