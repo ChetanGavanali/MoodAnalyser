b27fc62b4ebbfa986cfe1f44f3bcb64704d960ac
ChetanGavanali <chetangavanali@gmail.com>
2022-07-12 19:17:37 +0530

    Added MoodAnalyser UC1

---
+++ src/main/java/com/moodanalyser/MoodAnalyser.java
@@ -0,0 +1,22 @@
+package com.moodanalyser;
+
+public class MoodAnalyser {
+    public String moodAnalysis(String msg) {
+
+        if (msg.contains("sad")) {
+            return "Sad";
+        } else {
+            return "Happy";
+        }
+    }
+
+    public static void main(String[] args) {
+
+        MoodAnalyser moodAnalysis = new MoodAnalyser();
+        String sadMood = moodAnalysis.moodAnalysis("Iam in sad mood");
+        System.out.println("My Mood is: " + sadMood);
+        String happyMood = moodAnalysis.moodAnalysis("Iam in happy mood");
+        System.out.println("My Mood is: " + happyMood);
+
+    }
+}
