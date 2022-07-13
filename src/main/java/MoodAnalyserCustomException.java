c6c6fbce14d57c3fda1fca4f76436f1da9868150
ChetanGavanali <chetangavanali@gmail.com>
2022-07-13 18:57:27 +0530

    Added custom exceptions

---
+++ src/main/java/MoodAnalyserCustomException.java
@@ -0,0 +1,37 @@
+import com.moodanalyser.MoodAnalyserException;
+
+public class MoodAnalyserCustomException {
+
+    public String message;
+    String returnMessage;
+
+    public MoodAnalyserCustomException(String message)
+    {
+        this.message = message;
+    }
+
+    public String analyseMood() throws MoodAnalyserException {
+        try {
+            if (message.length() == 0) {
+                throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.EMPTY,
+                        "Please enter some thing!");
+            }
+            returnMessage = (message.contains("sad")) ? "SAD" : "HAPPY";
+        } catch (NullPointerException e) {
+            throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.NULL, "Please enter some thing!");
+        }
+        return returnMessage;
+    }
+
+    public static void main(String[] args) {
+
+        try {
+
+            MoodAnalyserCustomException moodAnalyzer = new MoodAnalyserCustomException(null);
+            String mood = moodAnalyzer.analyseMood();
+            System.out.println("My Mood is " + mood);
+        } catch (MoodAnalyserException e) {
+            System.out.println(e.type);
+        }
+    }
+}
